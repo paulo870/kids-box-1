822294e0798a46f5e8e4b8322d585489a5e6aed9
paulo870 <paulo@poliglotaangola.com>
2026-07-14 17:47:59 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -117,7 +117,7 @@ function loadStudentUnit(unitNumber) {
     }
 
     pageNumbers.forEach(num => {
-        currentImages.push(basePath + "page" + num + ".JPG");
+        currentImages.push(basePath + "page" + num + ".jpg");
     });
 
     loadImage(currentImages[currentIndex]);
@@ -153,7 +153,7 @@ function loadActivityUnit(unitNumber) {
     }
 
     pageNumbers.forEach(num => {
-        currentImages.push(basePath + "page" + num + ".JPG");
+        currentImages.push(basePath + "page" + num + ".jpg");
     });
 
     if (currentImages.length > 1) {

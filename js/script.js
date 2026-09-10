0b6d22ce041fb2225a7db848b2a2cf732aa79743
paulo870 <paulo@poliglotaangola.com>
2026-09-10 10:43:06 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -388,6 +388,7 @@ audioBtn.addEventListener("click", function(e) {
             audioPlayer.load();
             showAudioPlayer(); // <-- use the new function
             audioPlayer.controls = true;
+            audioPlayer.setAttribute("controlsList", "nodownload");
         });
 
         audioListContainer.appendChild(btn);
